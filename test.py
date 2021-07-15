4bba651dd5ba65d01e6c7ac0c8bcf4355f5854be
binchoo <079111w@gmail.com>
2021-07-15 12:55:21 +0900

    Add testcase: Genesis chapter 1.

--- test.py
+++ test.py
@@ -17,3 +17,15 @@ test_case("I still want to ask how you are doing.", "how you are doing")
 test_case("I will have order!", "order")
 
 test_case("We should look for a place to take shelter. I'll be fine, but we don't want you catching a cold.", "cold")
+
+mosquito="""
+A controversial plan is moving ahead to release genetically modified mosquitoes into the Florida Keys. More than 750,000 of the insects are set to be introduced there. Why? The aedes aegypti mosquito can carry dangerous diseases like the Zika virus, Dengue fever and yellow fever. Officials are looking for new ways to kill of these insects without using pesticides and genetically modified mosquitoes might be a way to do this. Only female mosquitoes bite people. The altered insects that are set to be released in Florida are male. They`ve been modified so that the female offspring they produce will die before they hatch from their eggs and grow big enough to bite people. And the company that developed these GMO mosquitoes say they`ve been very successful in controlling mosquito populations in Panama, Brazil and the Cayman Islands.
+"""
+
+test_case(mosquito, "controversial plan")
+
+genesis1="""
+In the beginning God created the heavens and the earth. 2 Now the earth was formless and empty, darkness was over the surface of the deep, and the Spirit of God was hovering over the waters. 3 And God said, “Let there be light,” and there was light. 4 God saw that the light was good, and he separated the light from the darkness. 5 God called the light “day,” and the darkness he called “night.” And there was evening, and there was morning—the first day. 6 And God said, “Let there be a vault between the waters to separate water from water.” 7 So God made the vault and separated the water under the vault from the water above it. And it was so. 8 God called the vault “sky.” And there was evening, and there was morning—the second day. 9 And God said, “Let the water under the sky be gathered to one place, and let dry ground appear.” And it was so. 10 God called the dry ground “land,” and the gathered waters he called “seas.” And God saw that it was good. 
+"""
+
+test_case(genesis1, "the heavens and the earth")
