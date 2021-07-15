bc12f6873d871a1e26d02fb0d4531472b822a4be
binchoo <079111w@gmail.com>
2021-07-15 22:39:10 +0900

    Rename model.py to modelconfig.py

--- resources/qg.py
+++ resources/qg.py
@@ -1,7 +1,6 @@
 from .nlp_aware import NLPAwareResource
 
 class GeneratedQuestionDto(NLPAwareResource):
-
     def get(self, baseKnowledge): 
         generated = self.nlp.offer(baseKnowledge + " </s>")
         return {"input": baseKnowledge, "generated": generated}
