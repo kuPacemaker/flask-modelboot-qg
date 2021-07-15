cf5de3cb657852aa65f8a24a1364ddca00459399
binchoo <079111w@gmail.com>
2021-07-16 01:08:08 +0900

    Use QG domain glossaries(words)

--- resources/qg.py
+++ resources/qg.py
@@ -1,14 +1,14 @@
 from .nlp_aware import NLPAwareResource
 from flask import request
 
-def response_wrapping(input_message, generated_message):
-    return { "input": input_message, "generated": generated_message }
+def response_wrapping(input, generated):
+    return { "input": input, "generated": generated }
 
 class GeneratedQuestionDto(NLPAwareResource):
     def get(self, baseKnowledge): 
-        generated = self.nlp.offer(baseKnowledge + " </s>")
+        generated_answer = self.nlp.offer(baseKnowledge + " </s>")
         responses = [
-            response_wrapping(baseKnowledge, generated)
+            response_wrapping(baseKnowledge, generated_answer)
         ]
         return {"responses": responses}
 
@@ -20,9 +20,9 @@ class BatchGeneratedQuestionDto(NLPAwareResource):
         except:
             return {"type": "error", "message": "required key not available."}
         
-        knowledges = [baseKnowledge + " </s>" for baseKnowledge in messages]
-        answers = self.nlp.offer(knowledges)
+        batch_baseKnowledge = [baseKnowledge + " </s>" for baseKnowledge in messages]
+        batch_generated_answer = self.nlp.offer(batch_baseKnowledge)
         responses = [
-            response_wrapping(baseKnowledge, generated_answer) for baseKnowledge, generated_answer in zip(knowledges, answers)
+            response_wrapping(baseKnowledge, generated_answer) for baseKnowledge, generated_answer in zip(batch_baseKnowledge, batch_generated_answer)
         ]
         return {"responses": responses}
