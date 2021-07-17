38f48e2dce716626b4d84e8e5126a5333c39724a
binchoo <079111w@gmail.com>
2021-07-17 17:09:43 +0900

    Don't add </s> tag at end of input sequence

--- resources/qg.py
+++ resources/qg.py
@@ -6,7 +6,7 @@ def response_wrapping(input, generated):
 
 class GeneratedQuestionDto(NLPAwareResource):
     def get(self, baseKnowledge): 
-        generated_answer = self.nlp.offer(baseKnowledge + " </s>")
+        generated_answer = self.nlp.offer(baseKnowledge)
         responses = [
             response_wrapping(baseKnowledge, generated_answer)
         ]
@@ -20,7 +20,7 @@ class BatchGeneratedQuestionDto(NLPAwareResource):
         except:
             return {"type": "error", "message": "required key not available."}
         
-        batch_baseKnowledge = [baseKnowledge + " </s>" for baseKnowledge in messages]
+        batch_baseKnowledge = [baseKnowledge for baseKnowledge in messages]
         batch_generated_answer = self.nlp.offer(batch_baseKnowledge)
         responses = [
             response_wrapping(baseKnowledge, generated_answer) for baseKnowledge, generated_answer in zip(batch_baseKnowledge, batch_generated_answer)
